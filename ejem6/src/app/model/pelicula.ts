b081d54df92a658b4536fab622e71e7bcc90836d
jminsausti <jminsausti@fptxurdinaga.com>
2017-12-04 18:35:33 +0100

    Update pelicula.ts

--- ejem6/src/app/model/pelicula.ts
+++ ejem6/src/app/model/pelicula.ts
@@ -1,8 +1,21 @@
 export class Pelicula{
-	constructor (
-		public id:number,
-		public titulo:string,
-		public director:string,
-		public anio:number){}
-	
+	public id:number;
+	public titulo:string;
+	public director:string;
+	public anio:number;
+
+	constructor (id:number, titulo:string, director:string,anio:number){
+		this.id= id;
+		this.titulo=titulo;
+		this.director=director;
+		this.anio=anio;
+	}
+  
+	//Hay una forma rápida de defnir una clase,
+	//y es definiendo directamento en los parámetros del constructor la palabra public
+	//quedaría así
+	//constructor(publi id:number,
+	//public titulo:string,
+	//public director:string,
+	//public anio:number){}
 }
